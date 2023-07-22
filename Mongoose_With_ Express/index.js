790c0c10968ab71c42608bcfbf6153090365213d
Adeleye06 <enochadeleye3@gmail.com>
2023-07-21 23:46:22 -0600

    .

--- Mongoose_With_ Express/index.js
+++ Mongoose_With_ Express/index.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require ('express');
 const app = express();
 const path = require('path');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/shopApp")
+  .connect("mongodb://127.0.0.1:27017/farmStand")
   .then(() => {
     console.log(" MONGO CONNECTION OPEN");
   })
@@ -12,14 +14,15 @@ mongoose
     console.log(err);
   });
 
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req , res) => {
-    res.send('WOOOOF');
-})
-app.listen(3000, () => {
-    console.log('APP IS LISTENING ON PORT 3000')
+app.get('/products', async(req, res) => {
+    const products = await Product.find({});
+    console.log(products);
+    res.render('products/index', {products})
 })
 
+app.listen(3000, (req, res) => {
+    console.log('LISTENING ON PORT 3000');
+})
