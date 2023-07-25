e0ceb295614ca00a3ccf6fcdf8a39b7a2f85a8fb
Adeleye06 <enochadeleye3@gmail.com>
2023-07-25 14:34:26 -0600

    still working

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -16,6 +16,7 @@ mongoose
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res) => {
     res.render('campgrounds/home')
 })
@@ -23,13 +24,25 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const allCampgrounds = await Campground.find({});
   res.render('campgrounds/index', {allCampgrounds});
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
 })
 
+app.post('/campgrounds', async(req, res) => {
+  const newCampground = new Campground(req.body.campground);
+  await newCampground.save();
+  res.redirect(`/campgrounds/${newCampground._id}`);
+})
 app.get('/campgrounds/:id', async(req, res) => {
   const {id} = req.params;
   const foundCampground = await Campground.findById(id);
   res.render('campgrounds/show', {foundCampground});
-} )
+});
+
+
+
 app.listen(3000, () => {
     console.log('Serving at port 3000');
 })
