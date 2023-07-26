05356312e8f52ca59a6cae1bd92f405c8a490da7
Adeleye06 <enochadeleye3@gmail.com>
2023-07-25 22:46:56 -0600

    full CRUD for yelp camp finally completed

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const path = require ('path');
 const app = express();
 const Campground = require('./models/campground')
+const methodOverride = require('method-override');
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
@@ -17,6 +18,7 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 app.get('/', (req, res) => {
     res.render('campgrounds/home')
 })
@@ -48,8 +50,17 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
   res.render('campgrounds/edit', {foundCampground});
 })
 
+app.put('/campgrounds/:id', async (req, res) => {
+  const {id} = req.params;
+  const updatedCampground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+  res.redirect(`/campgrounds/${updatedCampground._id}`);
+  });
 
-
+  app.delete('/campgrounds/:id', async(req, res) => {
+    const {id} = req.params;
+    const deletedCampground = await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+  })
 
 app.listen(3000, () => {
     console.log('Serving at port 3000');
