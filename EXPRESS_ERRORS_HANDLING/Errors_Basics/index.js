0de2a8e2973ef935baf613669c82d4db50d2b755
Adeleye06 <enochadeleye3@gmail.com>
2023-07-31 20:22:11 -0600

    still working

--- EXPRESS_ERRORS_HANDLING/Errors_Basics/index.js
+++ EXPRESS_ERRORS_HANDLING/Errors_Basics/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const AppError = require('./AppError')
 app.use(morgan('tiny'));
 app.use((req, res, next) => {
     req.requestTime = Date.now();
@@ -19,18 +19,11 @@ const verifyPassword = ((req, res, next) => {
     if(password === 'chickennuggets'){
         next();
     }
-  res.send('SORRY YOU NEED A PASSWORD');
-  throw new Error('Password required')
+    throw new AppError('password required', 401);
+  /* res.send('SORRY YOU NEED A PASSWORD'); */
+  /* throw new AppError(401,'Password required') */
 })
-/* app.use((req, res, next) => {
-    console.log('THIS IS MY FIRST MIDDLEWARE')
-    return next();
-})
-app.use((req, res, next) => {
-    console.log('THIS IS MY SECOND MIDDLEWARE');
-    next();
-})
- */
+
 app.get('/', (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('HOME PAGE');
@@ -51,12 +44,13 @@ app.use((req, res) => {
     res.status(404).send('NOT FOUND');
 })
 
+//error handling middleware
 app.use((err, req, res, next) => {
     console.log('**************************************');
     console.log('****************ERROR*****************');
     console.log('**************************************');
-
-    res.status(500).send('OH BOY WE GOT AN ERROR!');
+    console.log(err);
+    next(err);
 })
 app.listen(3000, () => {
     console.log('App is running on localhost:3000');
