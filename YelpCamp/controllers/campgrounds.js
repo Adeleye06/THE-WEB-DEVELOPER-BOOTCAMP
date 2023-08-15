b537c3af2b1dea43a92bb53c1c17292915d81882
Adeleye06 <enochadeleye3@gmail.com>
2023-08-15 16:58:07 -0600

    .

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+
 module.exports.index = async (req, res) => {
     const allCampgrounds = await Campground.find({});
     res.render("campgrounds/index", { allCampgrounds });
--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -7,22 +7,22 @@ const campgrounds = require('../controllers/campgrounds')
 //route to serve the form for a new campground
 router.get("/new", isLoggedIn,(campgrounds.renderNewForm));
   
-  //route to access all campgrounds
-  router.get("/", catchAsync(campgrounds.index));
-  
-  //route that adds the new campground to the database
-  router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));;
-  
-  router.get("/:id", catchAsync(campgrounds.showCampground));
-  
-  //this route serves the form in order for user to edit
-  router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.editCampground));
-  
-  //this route is to execute the PUT request from the form
-  router.put( "/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-  
-  //route to delete any campground
-  router.delete("/:id", isAuthor, catchAsync(campgrounds.deleteCampground));
+//route to access all campgrounds
+router.get("/", catchAsync(campgrounds.index));
+
+//route that adds the new campground to the database
+router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));;
+
+router.get("/:id", catchAsync(campgrounds.showCampground));
+
+//this route serves the form in order for user to edit
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.editCampground));
+
+//this route is to execute the PUT request from the form
+router.put( "/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
+
+//route to delete any campground
+router.delete("/:id", isAuthor, catchAsync(campgrounds.deleteCampground));
   
 
   module.exports = router;
